4f6da501281bb4eb3fdc8e7aa0a7595903f991f7
Paramjit Singh Baweja <58485121+ParamjitBaweja@users.noreply.github.com>
2021-07-30 17:25:48 +0530

    Add files via upload

---
+++ CircleDetection.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import cv2
+import numpy as np
+
+import cv2
+import numpy as np
+
+# Load image, grayscale, Otsu's threshold
+image = cv2.imread('wall.png')
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+
+# Find circles with HoughCircles
+circles = cv2.HoughCircles(thresh, cv2.HOUGH_GRADIENT, 1, minDist=150, param1=200, param2=8, minRadius=0)
+
+# Draw circles
+if circles is not None:
+    circles = np.round(circles[0, :]).astype("int")
+    for (x,y,r) in circles:
+        #cv2.circle(image, (x,y), r, (36,255,12), 2)
+	cv2.rectangle(image,(x-r,y+r),(x+r,y-r), (0,255,0), 1)
+
+cv2.imshow('thresh', thresh)
+cv2.imshow('image', image)
+cv2.waitKey()
+
+
